cd97b8e7953c5d7104cf1c738e17ac7cf65abd8c
Anna Tsarenko <atsarenko@uni-kassel.de>
2024-03-29 22:21:39 +0100

    ensure correct path and enhance error handling

--- jira-printer/cards_utils.py
+++ jira-printer/cards_utils.py
@@ -1,4 +1,7 @@
 import csv
+import os
+import sys
+import traceback
 
 import jinja2
 
@@ -16,7 +19,7 @@ def process_file(raw_jira_issues: str, name_to_save: str):
     except ValueError:
         return "Please do not use filters while exporting Jira issues. Try again with a file containing all fields."
     except Exception as err:
-        return "Unknown error occurred: " + str(err)
+        return "Unknown error occurred: " + traceback.format_exc()
 
 
 def _save_cards(file_name: str, cards_stream, cards_type: str):
@@ -34,7 +37,17 @@ def _read_cards(file_name: str):
 
 
 def _render_cards(jira_cards: list, template: str):
-    template_loader = jinja2.FileSystemLoader(searchpath="templates/")
+    current_working_directory = os.getcwd()
+    print(current_working_directory)
+    template_loader = jinja2.FileSystemLoader(searchpath=_resource_path("templates/"))
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template(template)
-    return template.render(cards=jira_cards)
+    return template.render(cards=jira_cards, path=_resource_path("icons/"))
+
+
+def _resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
