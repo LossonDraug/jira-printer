c3f17cb0c158542c85d0ea0808a1ee9cabf76810
Anna Tsarenko <atsarenko@uni-kassel.de>
2024-06-18 20:27:47 +0200

    add a posibility to read csv files in german

--- jira_printer/card.py
+++ jira_printer/card.py
@@ -2,9 +2,15 @@ import pypandoc
 
 COMPONENTS = "Components"
 
-WANTED_HEADERS = ["Issue Type", "Issue key", "Priority", "Summary", "Status",
-                  "Reporter", "Description", "Epic Link Summary", "Custom field (Story Points)",
+HEADERS_ENGLISH = ["Description", "Issue Type", "Issue key", "Priority", "Summary", "Status",
+                  "Reporter", "Epic Link Summary", "Custom field (Story Points)",
                   "Custom field (Story point estimate)"]
+
+HEADERS_GERMAN = ["Beschreibung", "Vorgangstyp", "Vorgangsschlüssel", "Priorität", "Zusammenfassung", "Status",
+                  "Autor", "Epic Link Zusammenfassung", "Benutzerdefinierte Felder (Story Points)",
+                  "Benutzerdefinierte Felder (Story Point-Schätzung)"]
+
+LANGUAGE_TO_HEADERS = {"en": HEADERS_ENGLISH, "de": HEADERS_GERMAN}
 
 
 class Cards:
@@ -12,12 +18,17 @@ class Cards:
     components_header_indices = []
     story_cards = []
     feature_cards = []
+    language = ""
 
     def __init__(self, headers: list):
         self.story_cards = []
         self.feature_cards = []
-        for header in WANTED_HEADERS:
-            self.wanted_headers[header] = headers.index(header)
+        # TODO add error handling
+        for lang, lang_headers in LANGUAGE_TO_HEADERS.items():
+            if lang_headers[0] in headers:
+                self.language = lang
+        for index, lang_header in enumerate(LANGUAGE_TO_HEADERS.get(self.language)):
+            self.wanted_headers[HEADERS_ENGLISH[index]] = headers.index(lang_header)
         self.components_header_indices = [i for i, header in enumerate(headers) if header == COMPONENTS]
 
     def add_card(self, values: list):
